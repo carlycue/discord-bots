19801cfde339d717078aee7620e4bca91142b73c
Carly <cxrlycue@gmail.com>
2020-09-07 14:53:54 -0600

    Update "Hello World Bot" to log on reconnecting event

--- hello-world-bot/app.js
+++ hello-world-bot/app.js
@@ -8,6 +8,10 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on("reconnecting", () => {
+  console.log(`${client.user.tag} is trying to reconnect`);
+});
+
 client.on("message", (msg) => {
   if (msg.content.toLowerCase() === "!hello") {
     msg.reply("Hello world!");
